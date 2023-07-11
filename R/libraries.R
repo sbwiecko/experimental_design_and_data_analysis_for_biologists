ac3dd5c5572b94b070ccc315ad36f0018ac527b3
mjkeough <107090242+mjkeough@users.noreply.github.com>
2023-07-11 12:07:01 +1000

    ex

--- R/libraries.R
+++ R/libraries.R
@@ -2,11 +2,12 @@
 # Load tidyverse to make things simple: "tidyverse", "broom", "broom.mixed"
 # Common lm add-ons: "car", "MASS", "Rmisc", "ez", "afex", "emmeans", "lme4", "lmerTest", "nlme", "VCA", "MuMIn", "lattice", "effectsize", "lmtest", 
 # Graphics enhancements: "ggExtra", "patchwork", "viridis", "ggsci", "ggforce"
-
+# Load tidyverse last, to make sure that dplyr loads after plyr. Can be some issues with the same function name appearing in both places, but doing different things. This is particularly an issue with operations by groups.
 # Package names
-packages <- c("knitr", "tidyverse", "broom", "broom.mixed", 
-              "car", "MASS", "Rmisc", "ez", "afex", "emmeans", "lme4", "lmerTest", "nlme", "VCA", "MuMIn", "lattice", "effectsize", "lmtest", 
-              "ggExtra", "patchwork", "viridis", "ggsci", "ggforce") 
+packages <- c("car", "MASS", "Rmisc", "ez", "afex", "emmeans", "lme4", "lmerTest", "nlme", "VCA", "MuMIn", "lattice", "effectsize", "lmtest", 
+              "ggExtra", "patchwork", "viridis", "ggsci", "ggforce",
+              "knitr", "tidyverse", "broom", "broom.mixed"
+              ) 
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
