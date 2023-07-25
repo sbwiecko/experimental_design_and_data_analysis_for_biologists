c9e35cf5252ab4fb24783eff182effd56474a628
mjkeough <107090242+mjkeough@users.noreply.github.com>
2023-07-25 11:06:43 +1000

    ex2

--- R/libraries.R
+++ R/libraries.R
@@ -2,12 +2,11 @@
 # Load tidyverse to make things simple: "tidyverse", "broom", "broom.mixed"
 # Common lm add-ons: "car", "MASS", "Rmisc", "ez", "afex", "emmeans", "lme4", "lmerTest", "nlme", "VCA", "MuMIn", "lattice", "effectsize", "lmtest", 
 # Graphics enhancements: "ggExtra", "patchwork", "viridis", "ggsci", "ggforce"
-# Load tidyverse last, to make sure that dplyr loads after plyr. Can be some issues with the same function name appearing in both places, but doing different things. This is particularly an issue with operations by groups.
+
 # Package names
-packages <- c("car", "MASS", "Rmisc", "ez", "afex", "emmeans", "lme4", "lmerTest", "nlme", "VCA", "MuMIn", "lattice", "effectsize", "lmtest", 
-              "ggExtra", "patchwork", "viridis", "ggsci", "ggforce",
-              "knitr", "tidyverse", "broom", "broom.mixed"
-              ) 
+packages <- c("knitr", "tidyverse", "broom", "broom.mixed", 
+              "car", "MASS", "Rmisc", "ez", "afex", "emmeans", "lme4", "lmerTest", "nlme", "VCA", "MuMIn", "lattice", "effectsize", "lmtest", 
+              "ggExtra", "patchwork", "viridis", "ggsci", "ggforce") 
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -15,4 +14,3 @@ if (any(installed_packages == FALSE)) {  install.packages(packages[!installed_pa
 
 # Packages loading
 invisible(lapply(packages, library, character.only = TRUE))
-
