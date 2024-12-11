8f3c74a2e54b1d68039ad15b9ee65b44e69518c0
mjkeough <107090242+mjkeough@users.noreply.github.com>
2024-12-11 19:40:39 +1100

    ch6 updates

--- R/libraries.R
+++ R/libraries.R
@@ -1,12 +1,12 @@
 # Just for  working with Rmarkdown; not needed for analysis: "knitr"
-# Load tidyverse to make things simple: "tidyverse", "broom", "broom.mixed"
-# Common lm add-ons: "car", "MASS", "Rmisc", "ez", "afex", "emmeans", "lme4", "lmerTest", "nlme", "VCA", "MuMIn", "lattice", "effectsize", "lmtest", 
+# Load tidyverse to make things simple: "tidyverse", "broom", "broom.mixed", "flextable"
+# Common lm add-ons: "car", "MASS", "Rmisc", "ez", "afex", "emmeans", "lme4", "lmerTest", "nlme", "VCA", "MuMIn", "lattice", "effectsize", "lmtest",
 # Graphics enhancements: "ggExtra", "patchwork", "viridis", "ggsci", "ggforce"
 
 # Package names
-packages <- c("knitr", "tidyverse", "broom", "broom.mixed", 
-              "car", "MASS", "Rmisc", "ez", "afex", "emmeans", "lme4", "lmerTest", "nlme", "VCA", "MuMIn", "lattice", "effectsize", "lmtest", 
-              "ggExtra", "patchwork", "viridis", "ggsci", "ggforce") 
+packages <- c("knitr", "tidyverse", "broom", "broom.mixed",
+              "car", "MASS", "Rmisc", "ez", "afex", "emmeans", "lme4", "lmerTest", "nlme", "VCA", "MuMIn", "lattice", "effectsize", "lmtest",
+              "ggExtra", "patchwork", "viridis", "ggsci", "ggforce")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
